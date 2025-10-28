cb2865a3d6649348a9cafffc382049e70b53db63
MohamedAbdrabou12 <mohamedabdrabou840@gmail.com>
2025-10-28 13:47:34 +0300

    add helper for Alimbic Async Migrations and dependancy for Fastapi Routes

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,19 +1,37 @@
 # app/db/session.py
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from contextlib import asynccontextmanager
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, AsyncEngine
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
-# Async engine
-engine = create_async_engine(
+# Create Async SQLAlchemy Engine
+engine: AsyncEngine = create_async_engine(
     settings.DATABASE_URL,
-    echo=True,  # optional: show SQL logs
-    future=True,
+    echo=True,  # Show SQL logs in console (optional)
+    future=True,  # Enable 2.0 style usage
 )
 
-# Async session factory
+# Create Async Session Factory
 async_session = sessionmaker(
     bind=engine,
     class_=AsyncSession,
     autoflush=False,
     expire_on_commit=False,
 )
+
+
+# Dependency for FastAPI Routes
+@asynccontextmanager
+async def get_db_session():
+    async with async_session() as session:
+        yield session
+
+
+# Helper for Alembic Async Migrations
+def get_engine() -> AsyncEngine:
+    return engine
+
+
+# # Helper to get a new session
+# def get_session() -> AsyncSession:    #we can use this in services
+#     return async_session()
